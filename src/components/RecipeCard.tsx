57a3b5ee0044b08a79d697c519e5f3c3187deec5
DAN <daniel.abad.noriega@gmail.com>
2026-02-04 10:47:35 +0100

    añadimos like a la receta
    solo visual

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,6 +1,7 @@
 import type { Recipe } from "../models/recipe"
 import { FaClock } from "react-icons/fa";
 import { FaSignal } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa6";
 
 interface RecipeCardProps {
     recipe: Recipe
@@ -12,6 +13,7 @@ const RecipeCard = ({recipe} : RecipeCardProps) => {
             <div className='img-container'>
                 <img src={recipe.image} />
                 <div className="category">{recipe.category}</div>
+                <FaHeart className="heart-icon"/>
             </div>
             <div className="card-body">
                 <div className='title'>{recipe.name}</div>
